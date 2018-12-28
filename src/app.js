f4526b069d061403764accca67579ef4918eb85e
krystian <spamowiec2@gmail.com>
2018-12-28 16:45:47 +0100

    Routing
    Routing and it's properties

--- src/app.js
+++ src/app.js
@@ -4,6 +4,14 @@ const express = require('express');
 
 const app = express();
 
+//accounts
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), { encoding:'UTF8'});
+const accounts = JSON.parse(accountData);
+
+//users
+const usersData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), { encoding:'UTF8'});
+const users = JSON.parse(usersData);
+
 //set the views directory
 app.set('views', path.join(__dirname, 'views')); //__dirname jako element z node-a
 app.set('view engine', 'ejs'); //use ejs as our view engine
@@ -11,8 +19,21 @@ app.set('view engine', 'ejs'); //use ejs as our view engine
 //tell express, how to find out public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+/** ROUTING **/
 //app.get - 2 parametry - url i callback
-app.get('/', (req, res) => res.render('index', { title: 'Index' }) );
+app.get('/',
+  (req, res) =>
+    res.render('index',
+      { title: 'Account Summary',
+        accounts: accounts }
+    )
+  );
+
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }) );
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }) );
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }) );
+
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }) );
 
 //Tworzenie serwera http
 app.listen(3000, () => console.log('Server is running on 3000'));
