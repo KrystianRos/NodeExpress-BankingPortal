7a1b5f40659d06d9a35038ea4bc99ae27f7be3f7
krystian <spamowiec2@gmail.com>
2018-12-29 14:22:54 +0100

    Routing - mod6

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
 const app = express();
 
 const { accounts, users, writeJSON } = require('./data');
@@ -12,6 +15,10 @@ app.set('view engine', 'ejs'); //use ejs as our view engine
 
 //tell express, how to find out public directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Routing
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 /** MIDDLEWARE **/
 app.use( express.urlencoded({ extended: true }) );
@@ -27,30 +34,7 @@ app.get('/',
     )
   );
 
-app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }) );
-app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }) );
-app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }) );
-
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }) );
-app.get('/transfer', (req, res) => res.render('transfer') );
-app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }) );
-
-// POST
-app.post('/transfer', (req, res) => {
-  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-
-  writeJSON();
-  res.render('transfer', { message: 'Transfer Completed' });
-} );
-app.post('/payment', (req, res) => {
-  accounts.credit.balance = accounts.credit.balance - req.body.amount;
-  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
-
-  writeJSON();
-  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
-});
-
 
 //Tworzenie serwera http
 app.listen(3000, () => console.log('Server is running on 3000'));
